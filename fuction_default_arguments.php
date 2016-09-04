745f53b161ef7901882fb0cd320c8fcec6f86c54
atonizer <atonizer@gmail.com>
2016-09-04 05:07:43 -0700

    Study PHP files

---
+++ fuction_default_arguments.php
@@ -0,0 +1,9 @@
+<?php
+
+function get_info($name, $title){
+    echo "$name has arrived, they are wit us as a $title";
+}
+
+get_info('James', 'spy');
+
+?>
--- functions.php
+++ functions.php
@@ -1,11 +1,69 @@
 <?php
 // Functions and Includes
-function greetings(){
-    echo "hello to you!<br/>";
+function greetings($name){
+    echo "hello to you, $name !<br/>";
 }
 
-function numbers($number){
+
+greetings('John Doe');
+
+/*function numbers($number){
 return $number + 2;
+}*/
+
+//greetings();
+
+
+?>
+
+<?php
+$current_user = "User";
+function is_user(){
+    global $current_user;
+    if($current_user == 'User'){
+        echo "It's User";
+    } else{
+        echo "This is not the User";
+    }
 }
 
+is_user();
+
+?>
+<br/>
+<?php
+function hello($arr){
+    if(is_array($arr)){
+        foreach($arr as $name){
+            echo "Hello, $name, how's it going!<br/>";
+        }
+    } else{
+        echo 'Hello, Everyone!';
+    }
+}
+
+$names = array(
+    'John',
+    'Joan',
+    'Jean',
+    'Jessy',
+    'Jacob',
+    'Joshua',
+    'Julia',
+    'Jayden',
+    'Jasmine',
+    'Jake',
+    'James',
+    'Joseph',
+    'Julian',
+    'Jeffrey',
+    'Jackson',
+    'Jasper',
+    'Jonah',
+    'Jude',
+    'Jose',
+    'Jesus'
+);
+
+hello($names);
 ?>
